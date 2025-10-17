d16842269f845a35827b20fce29e3a7df69cac5b
franpanteli <francescapanteli@gmail.com>
2025-10-17 20:56:08 +0100

    Automated commit on 2025-10-17 20:56:08

--- course_notes/03_MySQL_vs._SQL_Webpage_Notes.py
+++ course_notes/03_MySQL_vs._SQL_Webpage_Notes.py
@@ -1,3 +1,9 @@
 """ MySQL vs. SQL Webpage Notes
-
+    -> outline
+        -> introduction
+        -> MySQL vs SQL
+        -> what is SQL
+            -> what SQL is used for
+        -> what is MySQL
+            -> what is a relational database 
 """
