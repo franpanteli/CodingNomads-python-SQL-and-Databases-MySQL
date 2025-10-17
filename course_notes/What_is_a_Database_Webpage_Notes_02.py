88f31a6703a656a23f536ae9fa06b2d5913eb3b0
franpanteli <francescapanteli@gmail.com>
2025-10-17 01:21:46 +0100

    Automated commit on 2025-10-17 01:21:46

--- course_notes/What_is_a_Database_Webpage_Notes_02.py
+++ course_notes/What_is_a_Database_Webpage_Notes_02.py
@@ -1,3 +1,11 @@
 """ What_is_a_Database? Webpage Notes
-    -> 
+    -> outline
+        -> introduction
+        -> what is a database?
+        -> what can you do with a database?
+        -> uses for databases
+        -> databases vs spreadsheets
+        -> database components
+        -> different types of databases
+        -> the most
 """
