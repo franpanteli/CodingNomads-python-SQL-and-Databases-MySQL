919230f413e5bbd13e76458a2ec7359d022eda7b
franpanteli <francescapanteli@gmail.com>
2025-10-21 23:18:25 +0100

    Automated commit on 2025-10-21 23:18:25

--- course_notes/07_Installing_and_Connecting_to_MySQL_Workbench_Webpage_Notes_01.py
+++ course_notes/07_Installing_and_Connecting_to_MySQL_Workbench_Webpage_Notes_01.py
@@ -1,3 +1,16 @@
 """ Installing and Connecting to MySQL Workbench Webpage Notes
-
+    -> outline
+        -> introduction
+        -> what is MySQL Workbench
+        -> how to install MySQL Workbench
+        -> how to create a MySQL database connection
+        -> Summary: how to install MySQL Workbench and create a MySQL database connection
+    -> what is MySQL Workbench
+        -> 
+    -> how to install MySQL Workbench
+        ->
+    -> How to create a MySQL database connection
+        ->
+    -> Summary: how to install MySQL Workbench and create a MySQL database connection
+        ->
 """
