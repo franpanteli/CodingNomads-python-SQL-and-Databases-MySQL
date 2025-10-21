a6733a974b716c4b6a6b10ebd3507e00fe1b8314
franpanteli <francescapanteli@gmail.com>
2025-10-21 19:43:18 +0100

    Automated commit on 2025-10-21 19:43:17

--- course_notes/04_What_is_a_Database_Video_Notes.py
+++ course_notes/04_What_is_a_Database_Video_Notes.py
@@ -1,3 +1,11 @@
 """ Video: What is a Database? Video Notes
-
+    -> outline
+        -> what is a database
+        -> database types
+        -> the most popular databases
+        -> what is SQL
+        -> what is MySQL
+        -> MySQL vs SQL
+        -> how to use MySQL Workbench
+    -> 
 """
