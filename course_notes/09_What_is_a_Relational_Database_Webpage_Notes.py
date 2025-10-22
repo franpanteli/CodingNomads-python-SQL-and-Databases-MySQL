671d28ca4f1cdd551f4bf8747a9f6ed216335976
franpanteli <francescapanteli@gmail.com>
2025-10-22 20:59:27 +0100

    Automated commit on 2025-10-22 20:59:27

--- course_notes/09_What_is_a_Relational_Database_Webpage_Notes.py
+++ course_notes/09_What_is_a_Relational_Database_Webpage_Notes.py
@@ -9,4 +9,10 @@
         -> summary: what is a relational database?
     -> introduction
         -> 
+    -> what is a relational database?
+    -> relational databases design
+    -> relational schemas
+    -> relational database tables
+    -> example of relational database
+    -> summary: what is a relational database?
 """
