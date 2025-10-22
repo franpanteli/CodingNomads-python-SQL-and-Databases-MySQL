32c3ba814947b1bf60ad5c05e8daab4c9cc525c1
franpanteli <francescapanteli@gmail.com>
2025-10-22 18:39:29 +0100

    Automated commit on 2025-10-22 18:39:29

--- course_notes/08_Using_MySQL_Workbench_Video_Notes.py
+++ course_notes/08_Using_MySQL_Workbench_Video_Notes.py
@@ -1,3 +1,15 @@
 """ Using MySQL Workbench Video Notes
-
+    -> outline
+        -> using MySQL Workbrench
+        -> we are using the Sakila DB database
+        -> we don't have to have the Sakila database installed before watching the video
+            -> he does examples on this database in this video
+        -> we install the database in upcoming sessions (not this one)
+    -> SQL is for querying the database
+    -> MySQL is an application for RDBMS (Relational Database Management System)
+        -> this runs in the terminal
+        -> MySQL Workbench is the same thing, but with a GUI
+        -> we previously installed this
+        -> this required making a connection to a local host
+    -> 
 """
