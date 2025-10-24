230f72ccba0b2c02904a08ac1a462ee32303991a
franpanteli <francescapanteli@gmail.com>
2025-10-24 20:59:37 +0100

    Automated commit on 2025-10-24 20:59:36

--- course_notes/13_Creating_a_Simple_Relational_Database_Video_Notes.py
+++ course_notes/13_Creating_a_Simple_Relational_Database_Video_Notes.py
@@ -1,4 +1,10 @@
 """ Creating a Simple Relational Database Video Notes 
     -> outline
-        -> 
+        -> how to create a database using MySQL
+        -> using MySQL workbench
+        -> creating a schema
+        -> creating database tables
+        -> inserting new data
+        -> applying primary and foreign keys
+    -> 
 """
