b9d352c076d1c1885ae7a5ace0398396dfd4fe03
franpanteli <francescapanteli@gmail.com>
2025-10-24 20:53:34 +0100

    Automated commit on 2025-10-24 20:53:34

--- course_notes/12_Let's_Add_Some_Data!_Webpage_Notes.py
+++ course_notes/12_Let's_Add_Some_Data!_Webpage_Notes.py
@@ -65,4 +65,46 @@
         -> how AUTO_INCREMENT and CURRENT_TIMESTAMP work automatically
         -> how to query joined data from multiple tables
     -> this is an SQL script that inserts all of this data into the database:
+        USE SocialDB;
+
+        -- Insert Users
+        INSERT INTO Users (FirstName, LastName, Email) VALUES
+        ('Ed', 'Frinkel', 'ed@frinkel.co'),
+        ('Sally', 'Doogooder', 'sally@doogooder.co'),
+        ('George', 'Smith', 'george@smith.co');
+
+        -- Insert Images
+        INSERT INTO Images (ImageURL) VALUES
+        ('http://www.maravipost.com/wp-content/uploads/2017/07/computer-jokes-comic-book.jpg'),
+        ('https://i.pinimg.com/originals/a1/15/9f/a1159f251ddca8438c2bbb0b0bf70eaa.png'),
+        ('https://i.pinimg.com/originals/f2/3d/e2/f23de2ef2f598238e8f16bfb0c3c4cb5.jpg'),
+        ('https://i.pinimg.com/originals/f8/d4/b3/f8d4b3b32c3c18b4972b97f6474655a8.jpg');
+
+        -- Insert Posts
+        INSERT INTO Posts (UserId, PostText, ImageId) VALUES
+        (1, 'Ryan\'s First Post! Wahoo!', 4),
+        (2, 'Kim\'s First Post! Yay!', 2),
+        (3, 'Caden\'s First Post! Wow!', 3),
+        (4, 'Martin\'s on social media? :)', 1);
+
+        -- Insert Users_Friends relationships
+        INSERT INTO Users_Friends (UserId_1, UserId_2) VALUES
+        (1, 2),
+        (1, 3),
+        (1, 4),
+        (2, 3),
+        (3, 4);
+
+        -- Verify the data
+        SELECT * FROM Users;
+        SELECT * FROM Images;
+        SELECT * FROM Posts;
+        SELECT * FROM Users_Friends;
+
+        -- Example join query
+        SELECT u.FirstName, u.LastName, p.PostText, i.ImageURL
+        FROM Users u
+        JOIN Posts p ON p.UserId = u.UserId
+        JOIN Images i ON p.ImageId = i.ImageId
+        WHERE u.UserId = 1;
 """
