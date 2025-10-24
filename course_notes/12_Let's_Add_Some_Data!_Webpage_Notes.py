10e2f6f0cfcc61b89d76b2f7475d6b07f740d073
franpanteli <francescapanteli@gmail.com>
2025-10-24 20:07:58 +0100

    Automated commit on 2025-10-24 20:07:58

--- course_notes/12_Let's_Add_Some_Data!_Webpage_Notes.py
+++ course_notes/12_Let's_Add_Some_Data!_Webpage_Notes.py
@@ -1,3 +1,14 @@
 """ Let's Add Some Data! Webpage Notes 
-
+    -> this is a continuation of the previous SQL tutorial
+    -> this was the same as opening MySQL in the terminal and typing in the SQL commands in the previous set of notes
+    -> this is adding data to that database schema
+    -> outline
+        -> introduction
+        -> how to insert data using MySQL Workbench
+            -> select the database
+            -> select a table and click the add data icon
+            -> add values
+            -> validate changes, by clicking "Apply"
+            -> repeat this for each table
+        -> summary: using 'MySQL Insert Into', to add data into a table
 """
