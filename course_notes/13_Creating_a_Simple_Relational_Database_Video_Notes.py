dcf5630bbac340fe9a49e78181da59905246a3ec
franpanteli <francescapanteli@gmail.com>
2025-10-24 22:11:44 +0100

    Automated commit on 2025-10-24 22:11:44

--- course_notes/13_Creating_a_Simple_Relational_Database_Video_Notes.py
+++ course_notes/13_Creating_a_Simple_Relational_Database_Video_Notes.py
@@ -6,5 +6,9 @@
         -> creating database tables
         -> inserting new data
         -> applying primary and foreign keys
-    -> 
+    -> creating a relational database in MySQL Workbench
+    -> make the connection
+        -> you can click on the + buttons
+        -> 127.0.0.1 <- this is the hostname, the connection is hosted on our computer
+        -> the port name is   
 """
