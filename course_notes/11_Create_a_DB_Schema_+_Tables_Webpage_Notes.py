fc1a6343527547c3523afcd49648bb7080876f28
franpanteli <francescapanteli@gmail.com>
2025-10-24 02:09:30 +0100

    Automated commit on 2025-10-24 02:09:30

---
+++ course_notes/11_Create_a_DB_Schema_+_Tables_Webpage_Notes.py
@@ -0,0 +1,15 @@
+""" Create a DB Schema + Tables Webpage Notes 
+    -> outline
+        -> introduction
+        -> how to create a database schema in MySQL
+            -> a database schema refers to how the tables in a relational database are connected
+                -> e.g, by their columns
+        -> how to add a table using MySQL: create table
+            -> MySQL is an application for Relational Database Management (RDBMS)
+            -> the language it uses to query the database is SQL (Structured Query Language)
+            -> MySQL can be run in the CLI, or using an open-source application called MySQL Workbench
+                -> this contains a GUI
+        -> Summary: create a database schema and table in MySQL
+    -> introduction
+        -> 
+"""
