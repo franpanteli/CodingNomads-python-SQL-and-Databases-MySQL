63e71b1ddb5c0e1132f6d6dd7fdab6ea9b78c78c
franpanteli <francescapanteli@gmail.com>
2025-10-27 19:04:13 +0000

    Automated commit on 2025-10-27 19:04:13

--- course_notes/14_SELECT_Webpage_Notes.py
+++ course_notes/14_SELECT_Webpage_Notes.py
@@ -1,3 +1,12 @@
 """ SELECT Webpage Notes 
-
+    -> outline
+        -> introduction
+        -> key points on SQL syntax
+        -> select the database to use
+        -> how to use the SQL SELECT statement
+        -> SQL select statement examples
+            -> 1. Fetch everything
+            -> 2. Fetch multiple columns
+            -> 3. Fetch one distinct column
+        -> summary: SQL Select statement
 """
