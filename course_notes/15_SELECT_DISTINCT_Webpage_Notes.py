4c580076ddff4c8d83f47d0b12e40a23f946d2c2
franpanteli <francescapanteli@gmail.com>
2025-10-27 19:05:14 +0000

    Automated commit on 2025-10-27 19:05:14

--- course_notes/15_SELECT_DISTINCT_Webpage_Notes.py
+++ course_notes/15_SELECT_DISTINCT_Webpage_Notes.py
@@ -1,3 +1,7 @@
 """ SELECT DISTINCT Webpage Notes 
-
+    -> outline
+        -> introduction
+        -> how to use DISTINCT in SQL
+        -> SQL SELECT DISTINCT real-world example
+        -> summary: SQL SELECT DISTINCT statement
 """
--- course_notes/16_SQL_SELECT_+_SQL_SELECT_DISTINCT_Video_Notes.py
+++ course_notes/16_SQL_SELECT_+_SQL_SELECT_DISTINCT_Video_Notes.py
@@ -1,3 +1,4 @@
 """ SQL SELECT + SQL SELECT DISTINCT Video Notes 
-
+    -> outline
+        -> how to use the SQL SELECT and SQL SELECT DISTINCT statements
 """
