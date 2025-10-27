aa8e917482a96cd072f8a9c7ab9f6b3c25ca0f3c
franpanteli <francescapanteli@gmail.com>
2025-10-27 19:23:23 +0000

    Automated commit on 2025-10-27 19:23:23

--- course_notes/15_SELECT_DISTINCT_Webpage_Notes.py
+++ course_notes/15_SELECT_DISTINCT_Webpage_Notes.py
@@ -10,5 +10,9 @@
             for exmaple
         -> like Python sets, it removes the duplicates
     -> example
-        -> 
+        -> SELECT DISTINCT birth_day, birth_month FROM employees; <- what HR can use to extract birthdays
+            -> to select rows that have unique values in one or more columns
+    -> general syntax:
+        -> SELECT DISTINCT <YOUR_COLUMN_NAMES> FROM <YOUR_TABLE>;
+
 """
