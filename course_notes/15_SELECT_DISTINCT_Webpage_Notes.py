b770dbf7d216ac780c2069b470a3018007665a10
franpanteli <francescapanteli@gmail.com>
2025-10-27 19:21:21 +0000

    Automated commit on 2025-10-27 19:21:21

--- course_notes/15_SELECT_DISTINCT_Webpage_Notes.py
+++ course_notes/15_SELECT_DISTINCT_Webpage_Notes.py
@@ -4,4 +4,8 @@
         -> how to use DISTINCT in SQL
         -> SQL SELECT DISTINCT real-world example
         -> summary: SQL SELECT DISTINCT statement
+    -> SELECT DISTINCT <- to select rows that have unique data
+        -> if we want to know information about unique data
+        -> we can find out the number of distinct names in a table, if there are repeated first names in an actors table,
+            for exmaple
 """
