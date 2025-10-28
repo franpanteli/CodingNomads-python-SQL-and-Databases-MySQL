19c029c6f528c35dbdfd013ac09972d4e700defd
franpanteli <francescapanteli@gmail.com>
2025-10-28 23:56:53 +0000

    Automated commit on 2025-10-28 23:56:53

--- course_notes/18_AND,_OR,_NOT_Webpage_Notes.py
+++ course_notes/18_AND,_OR,_NOT_Webpage_Notes.py
@@ -10,5 +10,6 @@
         -> Summary: SQL AND, SQL OR, and SQL NOT queries
     -> AND
         -> this returns True if both results are True
-        -> 
+        -> SELECT * FROM film WHERE length < 50 AND rating = "G"; <- to look for a movie rated G
+            -> the length of the film is < 50 and the rating is G
 """
