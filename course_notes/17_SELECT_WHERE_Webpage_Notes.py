e80f73b2a151b2a6c1ab925f8e3b273e86a7feb4
franpanteli <francescapanteli@gmail.com>
2025-10-28 22:56:25 +0000

    Automated commit on 2025-10-28 22:56:25

--- course_notes/17_SELECT_WHERE_Webpage_Notes.py
+++ course_notes/17_SELECT_WHERE_Webpage_Notes.py
@@ -1,3 +1,10 @@
 """ SELECT WHERE Webpage Notes 
-
+    -> outline
+        -> introduction
+        -> what does the SQL SELECT WHERE statement do?
+        -> how to use WHERE in SQL
+            -> Example 1: SQL SELECT WHERE first name
+            -> Example 2: SELECT WHERE ID
+            -> Example 3: SELECT WHERE length
+        -> Summary: the SQL WHERE statement
 """
