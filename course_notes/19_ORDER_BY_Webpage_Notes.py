e8ecd455157d83f22ee91e8fcaf14e821252fcd6
franpanteli <francescapanteli@gmail.com>
2025-10-28 22:59:27 +0000

    Automated commit on 2025-10-28 22:59:27

--- course_notes/19_ORDER_BY_Webpage_Notes.py
+++ course_notes/19_ORDER_BY_Webpage_Notes.py
@@ -1,4 +1,10 @@
 """ ORDER BY Webpage Notes 
     -> outline
-        -> 
+        -> introduction
+        -> how does SQL ORDER BY work?
+        -> SQL ORDER BY examples
+            -> 1. SQL ORDER BY ascending
+            -> 2. SQL ORDER BY descending
+            -> 3. SQL ORDER BY field
+        -> Summary: SQL ORDER BY 
 """
