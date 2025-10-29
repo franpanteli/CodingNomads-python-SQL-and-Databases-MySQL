91fe06e4007378e11852b2a31bcf7fa8f107c7eb
franpanteli <francescapanteli@gmail.com>
2025-10-29 20:44:38 +0000

    Automated commit on 2025-10-29 20:44:38

--- course_notes/23_DELETE_Webpage_Notes.py
+++ course_notes/23_DELETE_Webpage_Notes.py
@@ -6,5 +6,12 @@
             -> 1. safe way to delete data
             -> 2. dangerous way to delete data
         -> summary: DELETE statement
-    -> 
+    -> TO REMOVE DATA FROM THE DATABASE
+    -> choose the table that contains the data
+    -> WE APPLY A BOOLEAN CONDITION TO SPECIFY WHAT DATA WE WANT TO REMOVE
+    -> examples
+        -> 1. safe way to delete data
+            -> IT'S SAFER TO APPLY A SPECIFIC WHERE CONDITION WHEN DELETING DATA FROM THE DATABASE
+            -> 
+        -> 2. dangerous way to delete data
 """
