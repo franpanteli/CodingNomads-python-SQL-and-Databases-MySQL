d6032f2aaed02e7e198d1e73842ffaefbf751707
franpanteli <francescapanteli@gmail.com>
2025-10-29 00:05:15 +0000

    Automated commit on 2025-10-29 00:05:15

--- course_notes/18_AND,_OR,_NOT_Webpage_Notes.py
+++ course_notes/18_AND,_OR,_NOT_Webpage_Notes.py
@@ -19,7 +19,12 @@
             _> selecting all of the film data, where the rating is G or PG (both of those conditions are True)
             -> this returns a lot more results than the AND query does, because there are more True statements on either
                 side than there are on both
-    -> OR and AND together
         -> SELECT * FROM sakila.film where rating = "G" OR rating = "PG" OR rating = "PG-13";
-        -> this uses three OR statements 
+        -> this uses three OR statements
+    -> NOT
+        -> THIS EXCLUDES RESULTS THAT MATCH AN EXPRESSION OR VALUE (NOT)
+            -> they aren't that certain value
+            -> NOT True is False, and vice versa
+            -> it returns the opposite
+        -> 
 """
