e333380a740266d86f51bfc3de3ee187186eb6e7
franpanteli <francescapanteli@gmail.com>
2025-10-29 00:24:25 +0000

    Automated commit on 2025-10-29 00:24:25

--- course_notes/20_LIMIT_Webpage_Notes.py
+++ course_notes/20_LIMIT_Webpage_Notes.py
@@ -7,4 +7,14 @@
             -> 2. Limit 5 from 2 tables
         -> avoid this error with SQL limit
         -> summary: SQL LIMIT query
+    -> this is a utility statement
+    -> THIS LIMITS THE NUMBER OF RESULTS WE WILL GET BACK
+    -> WE PLACE THIS AT THE END OF THE QUERY
+    -> LIMIT examples
+        -> LIMIT 10 1 from Table
+            -> we are using customer and payment tables
+            -> 
+        -> LIMIT 5 from 2 tables
+    -> avoid this error with LIMIT
+    -> summary
 """
