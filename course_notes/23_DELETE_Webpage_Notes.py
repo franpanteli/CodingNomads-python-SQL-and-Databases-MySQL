992f149433a63e22ad3e217f7876d9a9eaec4279
franpanteli <francescapanteli@gmail.com>
2025-10-29 20:45:38 +0000

    Automated commit on 2025-10-29 20:45:38

--- course_notes/23_DELETE_Webpage_Notes.py
+++ course_notes/23_DELETE_Webpage_Notes.py
@@ -11,7 +11,11 @@
     -> WE APPLY A BOOLEAN CONDITION TO SPECIFY WHAT DATA WE WANT TO REMOVE
     -> examples
         -> 1. safe way to delete data
-            -> IT'S SAFER TO APPLY A SPECIFIC WHERE CONDITION WHEN DELETING DATA FROM THE DATABASE
-            -> 
+            -> DELETE FROM sakila.actor
+            -> WHERE actor_id = 201;
+                -> IT'S SAFER TO APPLY A SPECIFIC WHERE CONDITION WHEN DELETING DATA FROM THE DATABASE
+                -> we can tell if this has worked or not, by looking at the number of rows under "Response" in MySQL
+                    Workbench
+                -> this tells you the amount of time the query took to run
         -> 2. dangerous way to delete data
 """
