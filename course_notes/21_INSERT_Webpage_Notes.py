320762348746b0e1ff96080488989978a2cba1b2
franpanteli <francescapanteli@gmail.com>
2025-10-29 00:35:31 +0000

    Automated commit on 2025-10-29 00:35:31

--- course_notes/21_INSERT_Webpage_Notes.py
+++ course_notes/21_INSERT_Webpage_Notes.py
@@ -1,3 +1,9 @@
 """ INSERT Webpage Notes 
-
+    -> outline
+        -> introduction
+        -> examples of SQL INSERT INTO
+            -> 1. insert two values in two columns
+            -> 2. insert one value into one column
+        -> summary: the INSERT INTO command
+    -> 
 """
