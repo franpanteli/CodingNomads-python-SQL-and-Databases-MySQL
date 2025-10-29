51ded354924eb24faae57bbf7067d7248e4a3224
franpanteli <francescapanteli@gmail.com>
2025-10-29 20:59:43 +0000

    Automated commit on 2025-10-29 20:59:43

--- course_notes/24_INNER_JOIN_Webpage_Notes.py
+++ course_notes/24_INNER_JOIN_Webpage_Notes.py
@@ -10,5 +10,31 @@
         -> INNER JOIN example - explained
             -> INNER JOIN tips
         -> summary: the INNER JOIN statement
-    ->
+    -> INNER JOIN
+        -> joining one or more tables together
+        -> INNER JOIN IS THE DEFAULT JOIN IN MYSQL
+        -> INNER JOIN SELECTS ALL RECORDS THAT HAVE MATCHING VALUES IN THE **TWO** CHOSEN TABLES
+    -> INNER JOIN example
+        -> 1. select your tables to join
+            -> CHOOSE THE TWO TABLES WE WANT TO JOIN
+            -> joining the `actor`, `film_actor` and `film` tables
+
+        -> 2. select the join columns
+            -> JOINING TABLES USING THEIR COLUMNS
+            -> THERE HAVE TO BE CORRESPONDING COLUMNS THAT HAVE CORRESPONDING VALUES
+                -> WE WANT TO JOIN THE TWO TABLES BY THESE VALUES
+            -> for example, the two tables we want to join contain the same ID's
+                -> we want to join them by this column
+
+        -> 3. optional: add a conditional statement
+            -> we can filter the results with a condition instead of getting more information than we need
+            -> this is done, by using the WHERE statement
+            -> selecting all films that a certain actor is in, for example
+
+        -> 4. run the query
+            -> 
+
+    -> INNER JOIN example - explained
+    -> INNER JOIN tips
+    -> summary: the INNER JOIN statement
 """
