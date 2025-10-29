719c398ee1d4f29d33647ac2d3ce0501646b09e5
franpanteli <francescapanteli@gmail.com>
2025-10-29 20:36:14 +0000

    Automated commit on 2025-10-29 20:36:14

--- course_notes/24_INNER_JOIN_Webpage_Notes.py
+++ course_notes/24_INNER_JOIN_Webpage_Notes.py
@@ -1,4 +1,14 @@
 """ INNER JOIN Webpage Notes 
     -> outline
-        -> in
+        -> introduction
+        -> what is INNER JOIN?
+        -> INNER JOI example
+            -> 1. select your tables to join
+            -> 2. select the join columns
+            -> 3. optional: add a conditional statement
+            -> 4. run the query
+        -> INNER JOIN example - explained
+            -> INNER JOIN tips
+        -> summary: the INNER JOIN statement
+    -> 
 """
