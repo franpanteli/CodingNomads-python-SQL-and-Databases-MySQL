f02311eae7109e64b0d00939b68e501397b5e2f0
franpanteli <francescapanteli@gmail.com>
2025-10-30 14:24:55 +0000

    Automated commit on 2025-10-30 14:24:55

--- course_notes/34_Working_with_SQL_Stored_Procedures_Webpage_Notes.py
+++ course_notes/34_Working_with_SQL_Stored_Procedures_Webpage_Notes.py
@@ -1,4 +1,18 @@
 """ Working with SQL Stored Procedures Webpage Notes 
     -> outline
-        -> 
+        -> introduction
+        -> the SQL stored procedure
+        -> how to create a stored procedure
+            -> 1. define stored procedure parameters
+            -> 2. run the code
+            -> 3. refresh the schema
+        -> stored procedure keywords
+            -> 1. create procedure
+            -> 2. begin
+            -> 3. end
+            -> 4. delimiter
+        -> calling a stored procedure
+        -> summary: stored procedures
+    ->
 """
+
