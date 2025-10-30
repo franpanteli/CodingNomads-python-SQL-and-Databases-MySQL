6653f7d9c56f2b5839b02306b8702687b9066abf
franpanteli <francescapanteli@gmail.com>
2025-10-30 14:19:51 +0000

    Automated commit on 2025-10-30 14:19:51

--- course_notes/30_LIKE_+_Wildcards_Webpage_Notes.py
+++ course_notes/30_LIKE_+_Wildcards_Webpage_Notes.py
@@ -1,3 +1,10 @@
 """ LIKE + Wildcards Webpage Notes 
-
+    -> outline
+        -> introduction
+        -> LIKE
+        -> wildcard characters
+            -> % wildcard
+            -> _ wildcard
+        -> summary: LIKE statement and wildcards
+    ->
 """
--- course_notes/31_IN_Webpage_Notes.py
+++ course_notes/31_IN_Webpage_Notes.py
@@ -1,3 +1,8 @@
 """ IN Webpage Notes 
-
+    -> outline
+        -> introduction
+        -> how does the IN statement work?
+        -> IN example
+        -> summary: IN statement
+    -> 
 """
